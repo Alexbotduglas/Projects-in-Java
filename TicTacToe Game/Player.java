ab0576e7b6092632941e6d68b343536405231a44
Alexbotduglas <52333919+Alexbotduglas@users.noreply.github.com>
2025-08-25 19:12:45 +0300

    Create Player.java

---
+++ TicTacToe Game/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private final String name;
+    private final char symbol; // 'X' или 'O'
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
